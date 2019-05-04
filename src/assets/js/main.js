b6c3b57af0a760f08a48109059e1e94567132d6f
Nikita <nikitossuv@gmail.com>
2019-05-04 19:18:37 +0300

    first commit

---
+++ src/assets/js/main.js
@@ -0,0 +1,11 @@
+/**
+ * Section: jQuery
+ */
+
+	//@@include('general/global/global.js')
+	//@@include('general/ui/ui.js')
+	//@@include('general/components/components.js')
+	//@@include('general/sections/sections.js')	
+
+
+
